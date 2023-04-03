08129487d847d4b594bdc537d69169f0df9c47c5
Sierra <maldonado.sierra.a@gmail.com>
2023-04-03 17:13:38 -0400

    homework

---
+++ AutoPopulateUsers.ps1
@@ -0,0 +1,27 @@
+# Sierra Maldonado
+# Auto Populate Users
+#https://social.technet.microsoft.com/Forums/ie/en-US/50bfd4e1-b856-490b-8dba-c4219e0f1b0f/newaduser-the-server-is-unwilling-to-process-the-request?forum=ITCG
+
+
+
+Param (
+    [parameter(mandatory=$true)]
+    [string]$UserPassword
+)
+
+# Filepath
+$Filepath = "C:\Users\Administrator\Documents\Clean-Power.csv"
+
+$pswrd = ConvertTo-SecureString $UserPassword -AsPlainText -Force
+$users = Import-CSV $Filepath
+foreach ($user in $users) {
+    $props = @{
+        Name = $User.Username + '\' + $User.LastName + $User.FirstName
+        DisplayName = $User.LastName + ' ' + $User.FirstName
+        EmailAddress = $User.Username + '@cleanpower.com'
+        Surname = $User.LastName
+        GivenName = $User.FirstName # Add GivenName property
+        AccountPassword = $pswrd
+    }
+    New-ADUser @props -PassThru
+}
