d0e97f2a25ded54e81ea27f48e076f5b7643d6dd
GenevaKnott <geneva.vitales@yahoo.com>
2023-04-03 21:06:23 +0000

    New User

--- OnboardScript_ActiveDriectory.ps1
+++ OnboardScript_ActiveDriectory.ps1
@@ -1,19 +1,20 @@
 #Geneva Knott 
-# Auto Delete User
+# Auto New User
 # https://blog.netwrix.com/2018/06/07/how-to-create-new-active-directory-users-with-powershell/
 # 3 April 2023
 
-# This code can be ran upon termination of an employee. 
+Import-Module ActiveDirectory
 
+# Prompt for user details
+$Name = Read-Host "Enter the full name of the user"
+$GivenName = Read-Host "Enter the user's first name"
+$Surname = Read-Host "Enter the user's last name"
+$SamAccountName = Read-Host "Enter the user's SamAccountName"
+$Email = Read-Host "Enter the user's email address"
+$Password = Read-Host "Enter the user's password" -AsSecureString
+$Enabled = Read-Host "Enable user account? (True/False)"
+
+# Create the new user
+New-ADUser -Name $Name -GivenName $GivenName -Surname $Surname -SamAccountName $SamAccountName -UserPrincipalName "$SamAccountName@$DomainName" -EmailAddress $Email -AccountPassword $Password -Enabled $Enabled
 # Retrieve the user
 Get-ADUser $SamAccountName
-
-# Prompt to delete the user
-$DeleteUser = Read-Host "Do you want to delete the user? (Yes/No)"
-if ($DeleteUser -eq "Yes") {
-    # Delete the user
-    Remove-ADUser -Identity $SamAccountName -Confirm:$false
-    Write-Host "User account $SamAccountName has been deleted."
-} else {
-    Write-Host "User account $SamAccountName has not been deleted."
-}
