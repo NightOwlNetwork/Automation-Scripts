a6a974e83cc6bef7bcf55f9f0715a525bcaf891e
Sierra <maldonado.sierra.a@gmail.com>
2023-04-03 17:36:46 -0400

    homework

--- AutoPopulateUsers.ps1
+++ AutoPopulateUsers.ps1
@@ -3,7 +3,6 @@
 #https://social.technet.microsoft.com/Forums/ie/en-US/50bfd4e1-b856-490b-8dba-c4219e0f1b0f/newaduser-the-server-is-unwilling-to-process-the-request?forum=ITCG
 
 
-
 Param (
     [parameter(mandatory=$true)]
     [string]$UserPassword
@@ -12,19 +11,18 @@ Param (
 # Filepath
 $Filepath = "C:\Users\Administrator\Documents\Clean-Power.csv"
 
-# Var
 $pswrd = ConvertTo-SecureString $UserPassword -AsPlainText -Force
 $users = Import-CSV $Filepath
-
-# Goes to CSV file and locates the Data then runs the NewADUser for each person
 foreach ($user in $users) {
     $props = @{
         Name = $User.Username + '\' + $User.LastName + $User.FirstName
         DisplayName = $User.LastName + ' ' + $User.FirstName
         EmailAddress = $User.Username + '@cleanpower.com'
         Surname = $User.LastName
-        GivenName = $User.FirstName # Add GivenName property
+        GivenName = $User.FirstName
         AccountPassword = $pswrd
+        Path = 'OU=' + $User.OU + ',DC=cleanpower,DC=com' # OU path based on CSV file
+        JobTitle = $User.JobTitle # add JobTitle attribute
     }
     New-ADUser @props -PassThru
 }
