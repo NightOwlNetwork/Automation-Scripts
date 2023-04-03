d691c5c99678c03ec8b1e4f5be8633ed7c6a81e1
GenevaKnott <geneva.vitales@yahoo.com>
2023-04-03 20:58:15 +0000

    New User

---
+++ OnboardScript_ActiveDriectory.ps1
@@ -0,0 +1,21 @@
+#Geneva Knott 
+# Auto New Users
+# https://blog.netwrix.com/2018/06/07/how-to-create-new-active-directory-users-with-powershell/
+# 3 April 2023
+
+
+Import-Module ActiveDirectory
+
+# Prompt for user details
+$Name = Read-Host "Enter the full name of the user"
+$GivenName = Read-Host "Enter the user's first name"
+$Surname = Read-Host "Enter the user's last name"
+$SamAccountName = Read-Host "Enter the user's SamAccountName"
+$Email = Read-Host "Enter the user's email address"
+$Password = Read-Host "Enter the user's password" -AsSecureString
+$Enabled = Read-Host "Enable user account? (True/False)"
+
+# Create the new user
+New-ADUser -Name $Name -GivenName $GivenName -Surname $Surname -SamAccountName $SamAccountName -UserPrincipalName "$SamAccountName@$DomainName" -EmailAddress $Email -AccountPassword $Password -Enabled $Enabled
+# Retrieve the user
+Get-ADUser $SamAccountName
