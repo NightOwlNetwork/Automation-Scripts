c69c382c542a23e42967f8d7ddd3e7220e38f7b1
Sierra <maldonado.sierra.a@gmail.com>
2023-04-05 11:08:36 -0400

    homework

--- AutoPopulateUsers.ps1
+++ AutoPopulateUsers.ps1
@@ -3,24 +3,30 @@
 #https://social.technet.microsoft.com/Forums/ie/en-US/50bfd4e1-b856-490b-8dba-c4219e0f1b0f/newaduser-the-server-is-unwilling-to-process-the-request?forum=ITCG
 # 03Apir23
 
-#Installs tools
+#Installs Tools
 Add-WindowsCapability -Name Rsat.ActiveDirectory.DS-LDS.tool~~~~0.0.1.0 -Online
 
 Import-Module ActiveDirectory
 
-#Finds CSV file
+#Finds CSV File
 $ADUser = Import-Csv "C:\Users\Administrator\Documents\CP.csv"
 
-# For each, reads each line as a new user
-Foreach ($User in $ADUser) {
+foreach ($User in $ADUser) {
+    # Check if user already exists
+    $existingUser = Get-ADUser -Filter "GivenName -eq '$($user.firstname)' -and Surname -eq '$($user.lastname)'" -ErrorAction SilentlyContinue
+    if ($existingUser) {
+        Write-Warning "User $($user.firstname) $($user.lastname) already exists in Active Directory."
+        continue
+    }
+
+    # Create new user account
     New-ADUser `
-        -Name "$($user.firstname)  $($user.lastname)" `
-        -Givenname $user.firstname `
+        -Name "$($user.firstname) $($user.lastname)" `
+        -GivenName $user.firstname `
         -Surname $user.lastname `
         -Enabled $true `
-        -Path 'OU=$($user.OU),DC=cleanpower,DC=com' `
+        -Path "OU=$($user.OU),DC=cleanpower,DC=com" `
         -Title $user.jobtitle `
         -Email $user.email `
-        -AccountPassword (ConvertTo-SecureString $user.password -AsPlaintext -Force)
+        -AccountPassword (ConvertTo-SecureString $user.password -AsPlainText -Force)
 }
-echo "New Users Added"
